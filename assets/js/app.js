b423a7909fccec674aeb6a4b9f06734d7757cf2f
Antonio <antoniopdspain@gmail.com>
2022-10-10 13:38:15 +0200

    añado mas iconos de logo FrikiGit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,38 @@
-const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorProductos = document.getElementById('products');
 
+const contenedorCarrito = document.getElementById('carrito-contenedor');
 
-stockProductos.forEach((producto) =>{
+let carrito = []
+
+stockProductos.forEach((products) =>{
     const div = document.createElement('div')
 
+const boton = document.getElementById(`add_to_card${products.id}`)
 
+boton.addEventListener('click', () => {
+    agregarAlCarrito(products.id)
+})
     
 })
+
+const agregarAlCarrito = (prodId) => {
+    const item = stockProductos.find ((prod) => prod.id === prodId)
+    carrito.push(item)
+    actualizarCarrito()
+}
+
+
+
+const actualizarCarrito = () => {
+    carrito-forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio: ${prod.nombre}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        contenedorCarrito.appendChild(div)
+    }) 
+}
