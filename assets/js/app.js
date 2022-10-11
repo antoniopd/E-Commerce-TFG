74d848f88610a83a54782f4cdc1175f9f98b06fb
Antonio <antoniopdspain@gmail.com>
2022-10-11 13:14:24 +0200

    creo Stock de la seccion productos y los paso a js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,74 @@
-const contenedorProductos = document.getElementById('products');
+// Creo la constante contenedorProductos y selecciono el id del div donde quiero pintar los productos
+const contenedorProductos = document.getElementById('contenedor-productos');
 
-const contenedorCarrito = document.getElementById('carrito-contenedor');
 
 let carrito = []
 
-stockProductos.forEach((products) =>{
-    const div = document.createElement('div')
-
-const boton = document.getElementById(`add_to_card${products.id}`)
-
-boton.addEventListener('click', () => {
-    agregarAlCarrito(products.id)
+// Hago un forEach del array del stock de los productos y los guardo en (producto)
+stockProductos.forEach((producto) => {
+// Con innerHTML pinto el html dentro la constante que declaré de la sección de productos.
+    contenedorProductos.innerHTML += `  
+    <div class="col-sm-6 col-lg-4" id="1">
+      <a href="#" class="d-block text-center mb-4">
+        <div class="product-list">
+          <div class="product-image position-relative">
+            <span class="sale">Rebaja</span>
+            <img
+              src=${producto.img[0]}
+              class="img-fluid product-image-first"
+              alt="products"
+            />
+            <img
+              src=${producto.img[1]}
+              class="img-fluid product-image-secondary"
+              alt="products"
+            />
+          </div>
+          <div class="product-name pt-3">
+            <h3 class="text-capitalize">${producto.desc}</h3>
+            <p class="mb-0 amount">10.00€ <del>${producto.precio}€</del></p>
+            <div class="py-1">
+              <span class="ti-star"></span>
+              <span class="ti-star"></span>
+              <span class="ti-star"></span>
+              <span class="ti-star"></span>
+              <span class="ti-star"></span>
+            </div>
+            <button type="button" class="add_to_card" id="add_to_card${producto.id}">AÑADIR🛒</button>
+          </div>
+        </div>
+      </a>
+    </div>    
+   
+   `
 })
-    
-})
-
-const agregarAlCarrito = (prodId) => {
-    const item = stockProductos.find ((prod) => prod.id === prodId)
-    carrito.push(item)
-    actualizarCarrito()
-}
 
 
+// const boton = document.getElementById(`add_to_card${products.id}`)
 
-const actualizarCarrito = () => {
-    carrito-forEach((prod) => {
-        const div = document.createElement('div')
-        div.className = ('productoEnCarrito')
-        div.innerHTML = `
-        <p>${prod.nombre}</p>
-        <p>Precio: ${prod.nombre}</p>
-        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
-        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
-        `
-        contenedorCarrito.appendChild(div)
-    }) 
-}
+// boton.addEventListener('click', () => {
+//     agregarAlCarrito(products.id)
+// })
+
+
+// const agregarAlCarrito = (prodId) => {
+//     const item = stockProductos.find((prod) => prod.id === prodId)
+//     carrito.push(item)
+//     actualizarCarrito()
+// }
+
+
+
+// const actualizarCarrito = () => {
+//     carrito - forEach((prod) => {
+//         const div = document.createElement('div')
+//         div.className = ('productoEnCarrito')
+//         div.innerHTML = `
+//         <p>${prod.nombre}</p>
+//         <p>Precio: ${prod.nombre}</p>
+//         <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+//         <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+//         `
+//         contenedorCarrito.appendChild(div)
+//     })
+// }
---
+++ assets/js/stock.js
@@ -0,0 +1,8 @@
+let stockProductos = [
+    {id: 1, nombre: "", tipo: "", cantidad: 1, desc: "Camiseta manga corta chica", precio: 10.00, talla: "", img: ['assets/img/chica1-frente970x1000.jpg','assets/img/chica1-espaldas1.jpg']},
+    {id: 2, nombre: "", tipo: "", cantidad: 1, desc: "Camiseta casual chico", precio: 15.00, talla: "", img: ['assets/img/chico-entero-frente970x1000.jpg','assets/img/chico-entero-espalda907x1000.jpg']},
+    {id: 3, nombre: "", tipo: "", cantidad: 1, desc: "Camisetas chica dos colores", precio: 15.00, talla: "", img: ['assets/img/chica2-frente970x1000.jpg','assets/img/chica2-espaldas.jpg.jpg']},
+    {id: 4, nombre: "", tipo: "", cantidad: 1, desc: "Camiseta chico gris", precio: 12.00, talla: "", img: ['assets/img/chico5-gris-frente970x100.jpg','assets/img/chico5-gris-espalda970x1000.jpg']},
+    {id: 5, nombre: "", tipo: "", cantidad: 1, desc: "Camiseta de chica negra", precio: 10.00, talla: "", img: ['assets/img/chica3-frente-blk970x1000.jpg','assets/img/chica3-espalda-blk970x1000.jpg']},
+    {id: 5, nombre: "", tipo: "", cantidad: 1, desc: "Camiseta de chico manga larga", precio: 12.50, talla: "", img: ['assets/img/chico3-frente970X1000.jpg','assets/img/chico3-espaldas970X1000.jpg']}    
+]
