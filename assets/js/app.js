0f8a78142ca50db977c667a6156a15e35f3cfe44
Antonio <antoniopdspain@gmail.com>
2022-10-12 19:11:29 +0200

    agrego contador del carrito

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,35 @@
 const contenedorProductos = document.getElementById('contenedor-productos');
 
 
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const botonVaciar = document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('precioTotal')
+
 let carrito = []
+
+// Para almacenar los datos en localStorage y no perder los items de carrito
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+    actualizarCarrito()
+  }
+})
+
+botonVaciar.addEventListener('click', () => {
+  carrito.length = 0
+  actualizarCarrito()
+})
 
 // Hago un forEach del array del stock de los productos y los guardo en (producto)
 stockProductos.forEach((producto) => {
+  const div = document.createElement('div')
+  div.classList.add('col-sm-6')
 // Con innerHTML pinto el html dentro de la constante que declaré de la sección de productos.
-    contenedorProductos.innerHTML += `  
-    <div class="col-sm-6 col-lg-4" id="1">
-      <a href="#" class="d-block text-center mb-4">
+    div.innerHTML = `  
+    
+      <a href="#exampleModal" class="d-block text-center mb-4">
         <div class="product-list">
           <div class="product-image position-relative">
             <span class="sale">Rebaja</span>
@@ -26,7 +47,7 @@ stockProductos.forEach((producto) => {
           </div>
           <div class="product-name pt-3">
             <h3 class="text-capitalize">${producto.desc}</h3>
-            <p class="mb-0 amount">10.00€ <del>${producto.precio}€</del></p>
+            <p class="mb-0 amount">${producto.precio}€&nbsp;&nbsp;<del>${producto.descuento}€</del$></p>
             <div class="py-1">
               <span class="ti-star active"></span>
               <span class="ti-star active"></span>
@@ -38,37 +59,66 @@ stockProductos.forEach((producto) => {
           </div>
         </div>
       </a>
-    </div>    
    
-   `
+      `
+      contenedorProductos.appendChild(div)
+
+    const boton = document.getElementById(`add_to_card${producto.id}`)
+
+    boton.addEventListener('click', () => {
+      agregarAlCarrito(producto.id)
+  })
+  
+
 })
 
 
-// const boton = document.getElementById(`add_to_card${products.id}`)
+const agregarAlCarrito = (prodId) => {
+  const existe = carrito.some(prod => prod.id === prodId)
 
-// boton.addEventListener('click', () => {
-//     agregarAlCarrito(products.id)
-// })
+  if (existe) {
+    const prod = carrito.map(prod =>{
+      if (prod.id === prodId) {
+        prod.cantidad++
+      }
+    })
+  }else{
+
+  
+
+    const item = stockProductos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    actualizarCarrito()    
+    console.log(carrito)
+}
+actualizarCarrito()
+}
+
+const eliminarDelCarrito = (prodId) => {
+  const item = carrito.find((prod) => prod.id === prodId)
+  const indice = carrito.indexOf(item)
+  carrito.splice(indice, 1)
+  actualizarCarrito()
+}
 
 
-// const agregarAlCarrito = (prodId) => {
-//     const item = stockProductos.find((prod) => prod.id === prodId)
-//     carrito.push(item)
-//     actualizarCarrito()
-// }
 
+const actualizarCarrito = () => {
+  contenedorCarrito .innerHTML = ""
 
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio: ${prod.precio}€</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="ti-trash"></i></button>
+        `
+        contenedorCarrito.appendChild(div)
 
-// const actualizarCarrito = () => {
-//     carrito - forEach((prod) => {
-//         const div = document.createElement('div')
-//         div.className = ('productoEnCarrito')
-//         div.innerHTML = `
-//         <p>${prod.nombre}</p>
-//         <p>Precio: ${prod.nombre}</p>
-//         <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
-//         <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
-//         `
-//         contenedorCarrito.appendChild(div)
-//     })
-// }
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    })
+    contadorCarrito.innerText = carrito.length
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+}
