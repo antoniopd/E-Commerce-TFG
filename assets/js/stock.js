f523ff7c8a6e4076af80e41949c2433323291219
antoniopd <antoniopdspain@gmail.com>
2023-04-23 22:24:22 +0200

    ok

--- assets/js/stock.js
+++ assets/js/stock.js
@@ -4,7 +4,7 @@ let stockProductos = [
     {id: 3, nombre: "Camiseta 2m", tipo: "", cantidad: 1, desc: "Camisetas chica dos colores", precio: 14.99, descuento: 18.00, img: ['assets/img/chica2-frente970x1000.webp','assets/img/chica2-espaldas.jpg.webp']},
     {id: 4, nombre: "Camiseta cg", tipo: "", cantidad: 1, desc: "Camiseta chico gris", precio: 11.99, descuento: 14.00, img: ['assets/img/chico5-gris-frente970x100.webp','assets/img/chico5-gris-espalda970x1000.webp']},
     {id: 5, nombre: "Camiseta mg", tipo: "", cantidad: 1, desc: "Camiseta de chica negra", precio: 9.99, descuento: 13.00, img: ['assets/img/chica3-frente-blk970x1000.webp','assets/img/chica3-espalda-blk970x1000.webp']},
-    {id: 6, nombre: "Camiseta ml", tipo: "", cantidad: 1, desc: "Camiseta de chico manga larga", precio: 12.99, descuento: 15.00, img: ['assets/img/chico3-frente970X1000.webp','assets/img/chico3-espaldas970X1000.webp']}
+    {id: 6, nombre: "Camiseta ml", tipo: "", cantidad: 1, desc: "Camiseta de chico manga larga", precio: 12.99, descuento: 15.00, img: ['assets/img/chico3-frente970X1000.webp','assets/img/chico3-espaldas970X1000.webp']} 
      
 ]
 let specialProductos = [
